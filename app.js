90896856dbc835a74ed95418d1a8fcca303e812a
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-04-26 19:47:22 +0500

    Maths Table

---
+++ app.js
@@ -0,0 +1,26 @@
+
+// ========================= Multiplication Table  ===========================
+
+let inputForm = document.querySelector("#tableForm");
+let input = document.querySelector("#Inp");
+let table = document.querySelector(".time-table");
+
+
+
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let tableOf = input.value;  // user value
+
+  table.innerHTML = "";   //html
+
+  for (let i = 1; i <= 12; i++) {
+    
+    const Ans = `${tableOf} x ${i} = ${tableOf * i}`;
+
+    table.innerHTML += `${Ans} <br>`;
+  }
+});
+
+
+
